e0fbf3e3ee7bb9984ff40d82d2cfa6543d1a756b
“kishlaychandan00” <kishlaychandan00@email.com>
2025-01-03 16:57:01 +0530

    adding config.js

--- formHandling/src/config.js
+++ formHandling/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_BASE_URL: "http://localhost:5000/api",
+    // API_BASE_URL: "http://localhost:5000/api",
+    API_BASE_URL: "https://peppercloudassignment.onrender.com",
   };
   
   export default config;
