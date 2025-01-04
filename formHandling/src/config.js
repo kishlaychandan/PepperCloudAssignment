55609457ede3e6df044be3e31b6d243ae75c5e4b
“kishlaychandan00” <kishlaychandan00@email.com>
2025-01-04 14:01:47 +0530

    updating view form

--- formHandling/src/config.js
+++ formHandling/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_BASE_URL: "http://localhost:5000/api",
-    // API_BASE_URL: "https://peppercloudassignment.onrender.com/api",
+    // API_BASE_URL: "http://localhost:5000/api",
+    API_BASE_URL: "https://peppercloudassignment.onrender.com/api",
   };
   
   export default config;
